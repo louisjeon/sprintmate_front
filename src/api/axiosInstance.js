e2f909f1ab430c4955485b02d51522bde01ce232
louisjeon <wjss5115@gmail.com>
2025-05-25 23:33:30 +0900

    add axios withCredentials

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -6,6 +6,7 @@ const axiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
 // Add a request interceptor to include the Authorization header
