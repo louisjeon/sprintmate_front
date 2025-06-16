aecd99656f6651d2a2239e42df559980d87cf5b0
louisjeon <wjss5115@gmail.com>
2025-06-16 19:52:07 +0900

    remove my page button

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -39,12 +39,12 @@ const Navbar = ({ className }) => {
       <div>
         {isAuthenticated ? (
           <div className="flex items-center space-x-4">
-            <button
+            {/* <button
               className="bg-white text-blue-600 px-4 py-2 rounded-full hover:bg-gray-100 transition"
               onClick={() => navigate("/profile")}
             >
               My Page
-            </button>
+            </button> */}
             <button
               className="bg-red-500 text-white px-4 py-2 rounded-full hover:bg-red-600 transition"
               onClick={handleLogout}
